d0cf39ea9c920ca2469d253f2bce3a5a1e0df809
Ching-Ting Huang <hct0704@gmail.com>
2018-11-18 14:46:16 -0500

    Initial Commit

---
+++ Metro Explorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metro Explorer
+//
+//  Created by hct0704 on 11/18/18.
+//  Copyright © 2018 hct0704. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
